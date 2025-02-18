98483c077f72d653e3a05161d161514fc4053cbf
Marcos Medina <mimc.software@gmail.com>
2025-02-18 19:45:44 +0000

    Add test

--- java/junit5/src/test/java/GridShould.java
+++ java/junit5/src/test/java/GridShould.java
@@ -1,2 +1,92 @@
-package PACKAGE_NAME;public class GridShould {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GridShould {
+	static Grid grid;
+
+	@BeforeEach
+	void setUp() {
+		grid = new Grid();
+	}
+
+	@Test
+	void get_3_for_width() {
+		assertEquals(3, grid.getWidth());
+	}
+
+	@Test
+	void get_3_for_height() {
+		assertEquals(3, grid.getHeight());
+	}
+
+	@Test
+	void get_a_position_of_the_grid() {
+		int coordinateRandomX = (int)(Math.random() * grid.getWidth());
+		int coordinateRandomY = (int)(Math.random() * grid.getHeight());
+
+		assertNull(grid.getPieceOf(coordinateRandomX, coordinateRandomY));
+	}
+
+	@Test
+	void set_a_piece_on_the_grid() {
+		GamePieces randomPiece = getRandomGamePiece();
+		grid.setAPiece(randomPiece, 0, 1);
+
+		assertEquals(grid.getPieceOf(0, 1), randomPiece);
+	}
+
+	@Test
+	void return_true_if_have_a_piece_on_the_coordinates() {
+		GamePieces randomPiece = getRandomGamePiece();
+		grid.setAPiece(randomPiece, 0, 1);
+
+		assertTrue(grid.haveAPieceIn(0, 1));
+	}
+
+	@Test
+	void return_false_if_havent_a_piece_on_the_coordinates() {
+		assertFalse(grid.haveAPieceIn(0, 1));
+	}
+
+	@Test
+	void return_false_if_the_grid_are_full() {
+		setAllPieces();
+		assertFalse(grid.haveFreeSpace());
+	}
+
+	@Test
+	void return_true_if_any_row_have_the_same_piece() {
+		setAllPieces();
+		assertTrue(grid.anyRowHaveTheSamePiece());
+	}
+
+	@Test
+	void return_true_if_any_column_have_the_same_piece() {
+		setAllPieces();
+		assertTrue(grid.anyColumnHaveTheSamePiece());
+	}
+
+	@Test
+	void return_true_if_any_diagonal_have_the_same_piece() {
+		setAllPieces();
+		assertTrue(grid.anyDiagonalHaveTheSamePiece());
+	}
+
+	private void setAllPieces() {
+		for (int x = 0; x < grid.getHeight(); x++) {
+			for (int y = 0; y < grid.getWidth(); y++) {
+				grid.setAPiece(GamePieces.X, x, y);
+			}
+		}
+	}
+
+	public static GamePieces getRandomGamePiece() {
+		GamePieces[] values = GamePieces.values(); // Obtiene todos los valores del enum
+		int randomIndex = ThreadLocalRandom.current().nextInt(values.length); // Genera un índice aleatorio
+		return values[randomIndex]; // Retorna el valor aleatorio
+	}
 }
--- java/junit5/src/test/java/TicTacToeShould.java
+++ java/junit5/src/test/java/TicTacToeShould.java
@@ -1,21 +1,103 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
+class TicTacToeShould {
+	static TicTacToe game;
 
-class ThingTest {
+	@BeforeAll
+	static void setUp() {
+		game = new TicTacToe();
+		game.start();
+	}
 
-    @Test
-    void fail() {
-        Thing thing = new Thing();
-        String value = thing.callForAction();
-        assertThat(value)
-                .isEqualTo("Food");
-    }
+	@Test
+	void start_a_game_with_two_players_with_diferents_pieces() {
+		Player[] players = game.getPlayers();
 
-    @Test
-    void it_should_not_fail() {
-        assertThat(42)
-                .isEqualTo(42);
-    }
+		assertNotSame(players[0].piece, players[1].piece);
+	}
+
+	@Test
+	void start_a_new_world_to_play() {
+		Grid grid = game.getGrid();
+
+		assertTrue(grid.getHeight() == 3 && grid.getWidth() == 3);
+	}
+
+	@Test
+	void put_a_piece_in_a_field_on_the_grid() {
+		Player[] players = game.getPlayers();
+		int coordinateX = 0;
+		int coordinateY = 1;
+
+		game.setAPiece(players[0], coordinateX, coordinateY);
+
+		assertTrue(game.haveAPieceIn(coordinateX, coordinateY));
+	}
+
+	@Test
+	void cant_put_a_piece_in_a_field_that_are_busy() {
+		Player[] players = game.getPlayers();
+		int coordinateX = 0;
+		int coordinateY = 1;
+		Grid grid = game.getGrid();
+
+		if (!grid.haveAPieceIn(coordinateX, coordinateY)) {
+			game.setAPiece(players[0], coordinateX, coordinateY);
+		}
+
+		assertTrue(game.haveAPieceIn(coordinateX, coordinateY));
+	}
+
+	@Test
+	void end_a_game_when_all_fields_are_busy() {
+		setAllPieces();
+
+		assertTrue(game.isFinished());
+	}
+
+	@Test
+	void not_end_a_game_when_have_free_space_on_the_grid() {
+		Grid grid = game.getGrid();
+		grid.setAPiece(GamePieces.X, 0, 0);
+
+		assertFalse(game.isFinished());
+	}
+
+	@Test
+	void when_start_a_new_game_one_random_player_start() {
+		Player[] players = game.getPlayers();
+
+		assertNotEquals(players[0].getTurn(), players[1].getTurn());
+	}
+
+	@Test
+	void game_is_over_when_one_player_take_all_fields_of_a_row() {
+		setAllPieces();
+		assertTrue(game.anyPlayerWinInARow());
+	}
+
+	@Test
+	void game_is_over_when_one_player_take_a_field_of_a_column() {
+		setAllPieces();
+		assertTrue(game.anyPlayerWinInAColumn());
+	}
+
+	@Test
+	void game_is_over_when_one_player_take_a_diagonal() {
+		setAllPieces();
+		assertTrue(game.anyPlayerWinInADiagonal());
+	}
+
+	private void setAllPieces() {
+		Grid grid = game.getGrid();
+
+		for (int x = 0; x < grid.getWidth(); x++) {
+			for (int y = 0; y < grid.getHeight(); y++) {
+				grid.setAPiece(GamePieces.X, x, y);
+			}
+		}
+	}
 }
